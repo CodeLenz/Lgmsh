bd5880d9933beb6a31c917c224f4bcd7690f82b2
Eduardo Lenz <eduardobarplenz@gmail.com>
2024-06-20 12:54:20 -0300

    Silence the library

--- test/test_import.jl
+++ test/test_import.jl
@@ -3,25 +3,29 @@
     # mesh
     filename = "testmesh1.msh"
 
-    # Number of nodes, node order and coordinates
-    nn, norder, coord = Lgmsh_import_coordinates(filename)
+    # Avoid printing gmsh output
+    redirect_stdout(open(tempname(), "w")) do
 
-    # Element types
-    etypes = Lgmsh_import_etypes(filename)
+        # Number of nodes, node order and coordinates
+        nn, norder, coord = Lgmsh_import_coordinates(filename)
 
-    # Elements of each type
-    for type in etypes
-        ne_type, number, connect =  Lgmsh_import_element_by_type(filename,type)
+        # Element types
+        etypes = Lgmsh_import_etypes(filename)
+
+        # Elements of each type
+        for type in etypes
+            ne_type, number, connect =  Lgmsh_import_element_by_type(filename,type)
+        end
+
+        # Physical groups
+        pgroups, pgnames = Lgmsh_import_physical_groups(filename)
+
+        # Entities associated to each physical group
+        for name in pgnames
+        entities = Lgmsh_import_entities_physical_group(filename,name)
+        end
+
     end
-
-    # Physical groups
-    pgroups, pgnames = Lgmsh_import_physical_groups(filename)
-
-    # Entities associated to each physical group
-    for name in pgnames
-       entities = Lgmsh_import_entities_physical_group(filename,name)
-    end
-
 end
 
  
