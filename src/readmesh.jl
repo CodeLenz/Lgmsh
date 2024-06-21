6ad0a3f5b3c6300fe98300a9c32f912095e9edd8
Eduardo Lenz <eduardobarplenz@gmail.com>
2024-06-21 14:21:04 -0300

    Do not print gmsh output

--- src/readmesh.jl
+++ src/readmesh.jl
@@ -3,68 +3,73 @@
 #
 function Readmesh(filename::String, elist::Vector{Int})
 
-    # Nodes and coordinates
-    nn, norder, coord = Lgmsh_import_coordinates(filename)
+    # To avoid printing gmsh output
+    redirect_stdout(open(tempname(), "w")) do
 
-    # Nodes per element
-    npe = Lgmsh_nodemap()
+        # Nodes and coordinates
+        nn, norder, coord = Lgmsh_import_coordinates(filename)
 
-    # Maximum number of nodes per element in this mesh
-    nnmax = maximum(npe[elist])
+        # Nodes per element
+        npe = Lgmsh_nodemap()
 
-    # Something very, very stupid
-    ne = 0
-    for e in elist
-        ne_e, _ = Lgmsh_import_element_by_type(filename,e,false)
-        ne += ne_e
-    end
+        # Maximum number of nodes per element in this mesh
+        nnmax = maximum(npe[elist])
 
-    # basic test
-    if ne==0
-       error("There are no elements of types $(elist) in this mesh")
-    end
-
-    # Allocate the array of connectivities
-    connect = zeros(Int64,ne,nnmax)
-
-    # Allocate the array of types
-    types = zeros(Int64,ne)
-
-    # Allocate an auxiliary vector with the 
-    # original tags
-    tags = zeros(Int64,ne) 
-
-    # Loop over elist AGAIN 
-    offset = 0
-    for e in elist
-
-        # Recover data from each type of element
-        ne_e, tags_e, connect_e = Lgmsh_import_element_by_type(filename,e,false)
-
-        # Skip if ne_e is zero (no element of this type)
-        if ne_e > 0 
-
-            # Number of nodes per element in this particular type
-            nne = npe[e]
-
-            # Append connectivities
-            connect[offset+1:offset+ne_e,1:nne] .= connect_e
-
-            # Append tags
-            tags[offset+1:offset+ne_e] .= tags_e
-
-            # Append types
-            types[offset+1:offset+ne_e] .= e 
-
-            # Adjust the offset
-            offset += ne_e
-
-        else
-            println("Warning: No elements of type $e in this file")
+        # Something very, very stupid
+        ne = 0
+        for e in elist
+            ne_e, _ = Lgmsh_import_element_by_type(filename,e,false)
+            ne += ne_e
         end
-            
-    end
 
+        # basic test
+        if ne==0
+        error("There are no elements of types $(elist) in this mesh")
+        end
+
+        # Allocate the array of connectivities
+        connect = zeros(Int64,ne,nnmax)
+
+        # Allocate the array of types
+        types = zeros(Int64,ne)
+
+        # Allocate an auxiliary vector with the 
+        # original tags
+        tags = zeros(Int64,ne) 
+
+        # Loop over elist AGAIN 
+        offset = 0
+        for e in elist
+
+            # Recover data from each type of element
+            ne_e, tags_e, connect_e = Lgmsh_import_element_by_type(filename,e,false)
+
+            # Skip if ne_e is zero (no element of this type)
+            if ne_e > 0 
+
+                # Number of nodes per element in this particular type
+                nne = npe[e]
+
+                # Append connectivities
+                connect[offset+1:offset+ne_e,1:nne] .= connect_e
+
+                # Append tags
+                tags[offset+1:offset+ne_e] .= tags_e
+
+                # Append types
+                types[offset+1:offset+ne_e] .= e 
+
+                # Adjust the offset
+                offset += ne_e
+
+            else
+                println("Warning: No elements of type $e in this file")
+            end
+                
+        end
+
+    end
+    
     return nn, coord, ne, types, connect, tags
 
 end
