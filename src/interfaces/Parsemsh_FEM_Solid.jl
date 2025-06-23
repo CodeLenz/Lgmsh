71f0cad78b46ef1b4d0959a4f1911773f4a4d35d
Eduardo Lenz <eduardobarplenz@gmail.com>
2025-06-23 15:33:00 -0300

    Update Parsemsh_FEM_Solid.jl
    More fixes ...

--- src/interfaces/Parsemsh_FEM_Solid.jl
+++ src/interfaces/Parsemsh_FEM_Solid.jl
@@ -417,7 +417,7 @@ function Parsemsh_FEM_Solid(meshfile::String,verbose=false)
         FC[ini:fim,3] .= val
 
         # Realoca a posição inicial
-        ini = fim
+        ini = fim + 1
 
     end #f
    
@@ -460,7 +460,7 @@ function Parsemsh_FEM_Solid(meshfile::String,verbose=false)
         FB[ini:fim,3] .= val
 
         # Realoca a posição inicial
-        ini =  fim
+        ini =  fim + 1
 
     end #f
    
@@ -511,7 +511,7 @@ function Parsemsh_FEM_Solid(meshfile::String,verbose=false)
         
 
         # Realoca a posição inicial
-        ini = fim
+        ini = fim + 1
 
     end #f
 
@@ -557,7 +557,7 @@ function Parsemsh_FEM_Solid(meshfile::String,verbose=false)
         AP[ini:fim,3] .= val
 
         # Realoca a posição inicial
-        ini = fim
+        ini = fim + 1
 
     end #f
 
